1f2b4d4046cc20f8d3917f291f226273410c1600
Abdoulwa85 <boureimasouleyabdoulwahabou@gmail.com>
2025-05-26 13:32:01 +0100

    modification du fichier array_intersection.c

--- array_intersection.c
+++ array_intersection.c
@@ -1,49 +1,38 @@
 #include <stdio.h>
-
-// Ce programme affiche les éléments communs entre deux tableaux.
-// Il évite d’afficher les doublons dans le résultat.
-
+// Affiche les éléments communs entre deux tableaux sans doublons
 int main() {
     int n1, n2, i, j;
+    int t1[100], t2[100]; // Taille du tableau
 
-    // Lire la taille du premier tableau
     scanf("%d", &n1);
-    int t1[n1];
-
-    // Lire les éléments du premier tableau
     for (i = 0; i < n1; i++) {
         scanf("%d", &t1[i]);
     }
 
-    // Lire la taille du deuxième tableau
     scanf("%d", &n2);
-    int t2[n2];
-
-    // Lire les éléments du deuxième tableau
     for (i = 0; i < n2; i++) {
         scanf("%d", &t2[i]);
     }
 
-    // Affichage des éléments communs (intersection)
-    printf("Intersection : ");
     for (i = 0; i < n1; i++) {
         for (j = 0; j < n2; j++) {
             if (t1[i] == t2[j]) {
-                // Vérifier si l’élément a déjà été affiché
-                int deja_affiche = 0;
+                // Vérifier si déjà affiché
+                int deja_vu = 0;
                 for (int k = 0; k < i; k++) {
                     if (t1[k] == t1[i]) {
-                        deja_affiche = 1;
+                        deja_vu = 1;
                         break;
                     }
                 }
-                if (!deja_affiche) {
+                if (!deja_vu) {
                     printf("%d ", t1[i]);
                 }
-                break; // Ne pas afficher plusieurs fois
+                break;
             }
         }
     }
 
+    printf("\n"); // Fin de ligne obligatoire
     return 0;
 } // Fin du programme
