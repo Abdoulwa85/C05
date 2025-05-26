2d17a9f0da37fe60c665334176a4f283c181d922
Abdoulwa85 <boureimasouleyabdoulwahabou@gmail.com>
2025-05-26 12:59:35 +0100

    modification du fichier merge_arrays.s

--- merge_arrays.c
+++ merge_arrays.c
@@ -1,37 +1,46 @@
 #include <stdio.h>
-‎// Ce programme permet de fusionne un tableaux dans un troisième
-‎int main() {
-‎    int n1, n2, i;
-‎
-‎    scanf("%d", &n1); // Taille du 1er tableau
-‎    int t1[n1];
-‎    for (i = 0; i < n1; i++) {
-‎        scanf("%d", &t1[i]);
-‎    }
-‎
-‎    scanf("%d", &n2); // Taille du 2e tableau
-‎    int t2[n2];
-‎    for (i = 0; i < n2; i++) {
-‎        scanf("%d", &t2[i]);
-‎    }
-‎
-‎    int fusion[n1 + n2];
-‎
-‎    // Copier les éléments du 1er tableau
-‎    for (i = 0; i < n1; i++) {
-‎        fusion[i] = t1[i];
-‎    }
-‎
-‎    // Copier les éléments du 2e tableau
-‎    for (i = 0; i < n2; i++) {
-‎        fusion[n1 + i] = t2[i];
-‎    }
-‎
-‎    // Affichage
-‎    printf("Tableau fusionné : ");
-‎    for (i = 0; i < n1 + n2; i++) {
-‎        printf("%d ", fusion[i]);
-‎    }
-‎
-‎    return 0;
-‎} // Fin du programme
+
+// Ce programme fusionne deux tableaux en un seul tableau final.
+
+int main() {
+    int n1, n2, i;
+
+    // Lire la taille du premier tableau
+    scanf("%d", &n1);
+    int t1[n1];
+
+    // Lire les éléments du premier tableau
+    for (i = 0; i < n1; i++) {
+        scanf("%d", &t1[i]);
+    }
+
+    // Lire la taille du second tableau
+    scanf("%d", &n2);
+    int t2[n2];
+
+    // Lire les éléments du second tableau
+    for (i = 0; i < n2; i++) {
+        scanf("%d", &t2[i]);
+    }
+
+    // Créer le tableau fusionné avec une taille suffisante
+    int fusion[n1 + n2];
+
+    // Copier les éléments du premier tableau dans le tableau fusionné
+    for (i = 0; i < n1; i++) {
+        fusion[i] = t1[i];
+    }
+
+    // Copier les éléments du second tableau à la suite
+    for (i = 0; i < n2; i++) {
+        fusion[n1 + i] = t2[i];
+    }
+
+    // Afficher le tableau fusionné
+    printf("Tableau fusionné : ");
+    for (i = 0; i < n1 + n2; i++) {
+        printf("%d ", fusion[i]);
+    }
+
+    return 0;
+} // Fin du programme
