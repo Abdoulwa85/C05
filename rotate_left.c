0e7b5125c0fea66cbb88c2b055e272e8bd2c0213
Abdoulwa85 <boureimasouleyabdoulwahabou@gmail.com>
2025-05-26 13:05:05 +0100

    modification du fichier rotate_left.c

--- rotate_left.c
+++ rotate_left.c
@@ -1,32 +1,38 @@
-‎#include <stdio.h>
-‎// Ce proggramme permet d'effectuer une rotation à gauche du tableau (le 1er élément va à la fin)
-‎int main() {
-‎    int n, i, temp;
-‎    scanf("%d", &n); // Taille du tableau
-‎    int tab[n];
-‎
-‎    // Lecture des éléments
-‎    for (i = 0; i < n; i++) {
-‎        scanf("%d", &tab[i]);
-‎    }
-‎
-‎    // Sauvegarder le premier élément
-‎    temp = tab[0];
-‎
-‎    // Décaler les éléments vers la gauche
-‎    for (i = 0; i < n - 1; i++) {
-‎        tab[i] = tab[i + 1];
-‎    }
-‎
-‎    // Placer le premier élément à la fin
-‎    tab[n - 1] = temp;
-‎
-‎    // Affichage du tableau après rotation
-‎    printf("Tableau après rotation : ");
-‎    for (i = 0; i < n; i++) {
-‎        printf("%d ", tab[i]);
-‎    }
-‎
-‎    return 0;
-‎} // Fin du programme
-‎
+#include <stdio.h>
+
+// Ce programme effectue une rotation à gauche d’un tableau d'entiers.
+// Le premier élément est déplacé à la fin, et les autres sont décalés vers la gauche.
+
+int main() {
+    int n, i, temp;
+
+    // Lire la taille du tableau
+    scanf("%d", &n);
+
+    // Déclarer le tableau avec n éléments
+    int tab[n];
+
+    // Lire les n éléments du tableau
+    for (i = 0; i < n; i++) {
+        scanf("%d", &tab[i]);
+    }
+
+    // Sauvegarder le premier élément avant de le déplacer
+    temp = tab[0];
+
+    // Décaler tous les éléments vers la gauche
+    for (i = 0; i < n - 1; i++) {
+        tab[i] = tab[i + 1];
+    }
+
+    // Placer le premier élément à la fin
+    tab[n - 1] = temp;
+
+    // Afficher le tableau après la rotation
+    printf("Tableau après rotation : ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", tab[i]);
+    }
+
+    return 0;
+} // Fin du programme
